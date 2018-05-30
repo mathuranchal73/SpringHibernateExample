09453c94ecfedc9c4cd8fec4b1e8fc540efba04e
amathur2 <amathur2@LH7U05CG7461PSK.uhc.com>
2018-05-30 17:07:15 +0530

    latest

--- src/main/java/com/websystique/springmvc/configuration/AppConfig.java
+++ src/main/java/com/websystique/springmvc/configuration/AppConfig.java
@@ -12,7 +12,7 @@ import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = "com.websystique.springmvc.configuration")
+@ComponentScan(basePackages = "com.websystique.springmvc")
 public class AppConfig {
 	
 	@Bean
